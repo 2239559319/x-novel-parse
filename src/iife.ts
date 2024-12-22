3b5b0d657adb8ae4f59d01028019b3d38022cbc5
2239559319 <w2239559319@outlook.com>
2024-12-22 22:24:42 +0800

    update version

--- src/iife.ts
+++ src/iife.ts
@@ -1,9 +1,9 @@
 import { parseContent } from './text';
 import { parseCatalog } from './catalog';
 
-// const catalog = parseCatalog(document);
-// console.log('catalog');
-// console.log(catalog);
+const catalog = parseCatalog(document);
+console.log('catalog');
+console.log(catalog);
 
 const content = parseContent(document);
 console.log('content');
