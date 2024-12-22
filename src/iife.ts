ac482eb8cc6a7c23cc096b19a3c7db1e3cf1774d
2239559319 <w2239559319@outlook.com>
2024-12-22 16:32:53 +0800

    fix: sourcemap

--- src/iife.ts
+++ src/iife.ts
@@ -1,5 +1,10 @@
 import { parseContent } from './text';
 import { parseCatalog } from './catalog';
 
-const res = parseCatalog(document);
-console.log(res);
+const catalog = parseCatalog(document);
+console.log('catalog');
+console.log(catalog);
+
+const content = parseContent(document);
+console.log('content');
+console.log(content);
