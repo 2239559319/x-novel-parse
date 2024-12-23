328fd9ec192d30946bf1109cb7acc4ea2b59c938
2239559319 <w2239559319@outlook.com>
2024-12-23 09:22:02 +0800

    init benchmark

--- src/text.ts
+++ src/text.ts
@@ -10,8 +10,26 @@ export function parseContent(doc: Document) {
 
   const textsNodes = iTree.getSiblingNodes();
 
-  return textsNodes
-    ?.reverse()
-    .map((node) => node.textContent.trim())
-    .join('\n');
+  const { trim: trimArr, raw: rawArr } = textsNodes
+    .reverse()
+    .reduce<{ trim: string[]; raw: string[] }>(
+      (acc, node: Node) => {
+        const rawText = node.textContent;
+        const trimText = rawText.trim();
+
+        acc.raw.push(rawText);
+        acc.trim.push(trimText);
+        return acc;
+      },
+      { trim: [], raw: [] },
+    );
+
+  const trimText = trimArr.join('\n');
+  const rawText = rawArr.join('\n');
+  return {
+    trimArr,
+    rawArr,
+    trimText,
+    rawText,
+  };
 }
